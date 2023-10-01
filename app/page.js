1da3889e8f7c3ec8d0b61ebb7dd4f47f11d0737b
AshwithaRajagopal <113119547+vini-raj97@users.noreply.github.com>
2023-10-01 15:36:11 -0600

    nextkit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <h1>NEXT.js Starter Kit</h1>
+        <p>for starting new next projects</p>
+      </header>
+    </main>
+  )
+}
